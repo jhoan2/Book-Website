ab12a4e32d30a00c2d1e31ae54fe7cad1605d1be
John Hoang <67968051+jhoan2@users.noreply.github.com>
2020-11-20 19:44:11 -0600

    Revert "react router"

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,51 @@
 import React from 'react';
-import Landingpage from './components/views/Landingpage';
-import Login from './components/views/Login';
-import { Route, Switch } from "react-router-dom";
-import Signup from './components/views/Signup';
+import Header from './components/Header/Header';
+import CardItem from './components/card/CardItem';
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Footer from './components/Footer/Footer';
+
+const useStyles = makeStyles((theme) => ({
+    gridContainer: {
+        paddingLeft: '50px',
+        paddingRight: '50px',
+    },
+}));
 
 const App = () => {
-    
+    const classes = useStyles();
     return (
         <div>
-            <Switch>
-                <Route path='/signup'><Signup /></Route>
-                <Route path='/login'><Login /></Route>
-                <Route path='/'><Landingpage /></Route>
-            </Switch>
-
+            <Header />
+            <br/>
+            <Grid container spacing={3} className={classes.gridContainer}>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <CardItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <CardItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <CardItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <CardItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <CardItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <CardItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <CardItem />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <CardItem />
+                </Grid>
+            </Grid>
+            <br />
+            <Footer />
         </div>
 
     )};
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,31 +2,20 @@ import React from 'react'
 import { Toolbar, Typography, IconButton, Button, ButtonGroup } from '@material-ui/core';
 import './header.css';
 import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
-import { Link } from  "react-router-dom";
 
 const Header = () => {
     return (
         <div>
                 <Toolbar variant='regular' className='toolbar'>
-                    <Link to='/'>
-                        <IconButton >
-                            <LibraryBooksIcon className='homeIcon' />
-                        </IconButton>
-                    </Link>
+                    <IconButton >
+                        <LibraryBooksIcon className='homeIcon' />
+                    </IconButton>
                     <Typography variant='h6' >
                         Book-Website
                     </Typography>
                         <ButtonGroup size='large' className='headerButtons' variant='contained'>
-                                <Link to='/login'>
-                                    <Button className='loginButton'>
-                                            Login
-                                    </Button>        
-                                 </Link>
-                                 <Link to='/signup'>
-                                    <Button className='signUpButton'>
-                                        Sign Up
-                                    </Button>
-                                 </Link>
+                            <Button className='loginButton'>Login</Button>
+                            <Button className='signUpButton'>Sign Up</Button>
                         </ButtonGroup>
                 </Toolbar>                
         </div>
